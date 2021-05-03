931898b6e9d72ee345bdc997bb481fe4ecd24585
Artur <artur@kedzior.es>
2021-05-03 18:26:10 +0200

    multiple validators

--- BlazorFluentValidation/Shared/Address.cs
+++ BlazorFluentValidation/Shared/Address.cs
@@ -4,21 +4,16 @@ namespace BlazorFluentValidation.Shared
 {
     public class Address
     {
-        public string Line1 { get; set; }
-        public string Line2 { get; set; }
-        public string Town { get; set; }
-        public string County { get; set; }
-        public string Postcode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 
     public class AddressValidator : AbstractValidator<Address>
     {
         public AddressValidator()
         {
-            RuleFor(p => p.Line1).NotEmpty().WithMessage("You must enter Line 1");
-            RuleFor(p => p.Town).NotEmpty().WithMessage("You must enter a town");
-            RuleFor(p => p.County).NotEmpty().WithMessage("You must enter a county");
-            RuleFor(p => p.Postcode).NotEmpty().WithMessage("You must enter a postcode");
+            RuleFor(p => p.City).NotEmpty().WithMessage("You must enter a city");
+            RuleFor(p => p.Country).NotEmpty().WithMessage("You must enter a county");
         }
     }
 }
